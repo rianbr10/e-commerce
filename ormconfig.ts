b4ac6411c907dee38f79dd9029ff5252e5d81e2a
Rian Brunelli <rian10brunelli@gmail.com>
2022-08-03 20:06:46 -0300

    Typeorm configurado

---
+++ ormconfig.ts
@@ -0,0 +1,17 @@
+module.exports = {
+    "type": "postgres",
+    "host": "localhost",
+    "port": process.env.TYPEORM_PORT,
+    "username": process.env.TYPEORM_USERNAME,
+    "password": process.env.TYPEORM_PASSWORD,
+    "database": process.env.TYPEORM_DATABASE,
+    "entities": [
+        "src/app/models/*.ts"
+    ],
+    "migrations": [
+        "src/database/migrations/*.ts"
+    ],
+    "cli": {
+        "migrationsDir": "src/database/migrations"
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 
+import './database/connect';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+const port = process.env.PORT;
 
-app.listen(3000, () => console.log('Server started at http://localhost/3000'));
+app.listen(port, () => console.log(`Server started at http://localhost/${port}`));
